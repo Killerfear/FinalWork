42f5a0000bdd4fe9bc338bc437b36963e3fd7286
killerfear <592153670@qq.com>
2016-02-13 17:49:55 +0800

    修改数据，获取数据列表

--- routes/problemset.js
+++ routes/problemset.js
@@ -21,6 +21,7 @@ var LogicHandler = require('../lib/logic-handler');
 var mongo = require('../lib/mongo-extend');
 
 
+
 //新增题目描述
 router.post('/description/add', function(req, res, next) {
 	LogicHandler.Handle('index', req, res, next, co.wrap(function * () {
@@ -88,6 +89,47 @@ router.post('/description/update', function(req, res, next) {
 	}));
 });
 
+//获取题目数据列表
+router.get('/samplelist', function(req, res, next) {
+	LogicHandler.Handle('index', req, res, next, co.wrap(function * () {
+		var user = req.user;
+		if (!user.isAdmin) throw { message: "无权限" }
+
+		var problemId = req.query.problemId;
+		var files = fs.readdirSync(__dirname + "/problem/" + problemId);
+
+		if (!_.isArray(files)) throw files;
+
+		var dataFile = [];
+		files.forEach(function(file) {
+			if (!file.match(/.*\.(in|out)/)) return;
+			dataFile.push(file);
+		});
+
+		return { title: dataFile };
+	}));
+});
+
+//获取题目数据
+router.get('/sample', function(req, res, next) {
+	LogicHandler.Handle('index', req, res, next, co.wrap(function * () {
+		var user = req.user;
+		if (!user.isAdmin) throw { message: "无权限" };
+
+		var problemId = req.query.problemId;
+		var fileName = req.query.name;
+
+		var filePath = __dirname + "/problem/" + problemId + "/" + fileName;
+
+		if (!fs.existsSync(filePath)) throw { message: filePath + " 不存在" }
+
+		var text = fs.readFileSync(filePath, 'utf8');
+
+		return { title: text };
+	}));
+	
+});
+
 //添加题目测试数据
 router.post('/sample/add', upload.single('file'), function(req, res, next) {
 	LogicHandler.Handle('index', req, res, next, co.wrap(function * () {
@@ -108,8 +150,35 @@ router.post('/sample/add', upload.single('file'), function(req, res, next) {
 
 		return { title : req.baseUrl + req.path }
 	}));
-	
 });
+
+//修改题目测试数据
+router.post('/sample/update', function(req, res, next) {
+	var user = req.user;
+
+	if (!user.isAdmin) throw { message: "无权限" }
+
+	var problemId = req.body.problemId;
+	var name = req.body.fileName;
+	var data = req.body.data;
+
+	var filePath = __dirname + "/problem/" + problemId + "/" + name;
+
+	var err;
+	if (fs.existsSync(filePath) {
+		err = fs.writeFileSync(filePath, data, 'utf-8');
+	} else {
+		err = { message: "文件不存在" };
+	}
+
+	if (err) throw err;
+
+	return { title: req.baseUrl + req.path };
+});
+
+
+
+
 
 //删除题目测试数据
 router.post('/sample/delete', function(req, res, next) {
@@ -121,7 +190,7 @@ router.post('/sample/delete', function(req, res, next) {
 		var id = req.body.problemId;
 		var name = req.body.file;
 
-		var filePath = './problem/' + id + '/' + name;
+		var filePath = __dirname + '/problem/' + id + '/' + name;
 		var err = fs.unlinkSync(filePath);
 
 		if (err) throw { message: "删除文件失败", err: err };
